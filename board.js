c26240b8ff761eef09d0aaf9180edc7e4f9e36e6
Gaashi <93555759+GaashiShiro@users.noreply.github.com>
2023-07-07 14:54:24 +0300

    first commit

---
+++ board.js
@@ -0,0 +1,86 @@
+export class Board {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.tiles = Array(width * height).fill(0);
+      this.elem = document.getElementById("board");
+      this.currentPlayer = 1;
+    }
+  
+    draw() {
+        this.elem.innerHTML = "";
+        this.elem.style.setProperty("--width", this.width);
+        this.elem.style.setProperty("--height", this.height);
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                const tileValue = this.tiles[y * this.width + x];
+                const tileElem = document.createElement("div");
+                tileElem.dataset.x = x;
+                tileElem.dataset.y = y;
+                tileElem.classList.add("tile");
+
+                if (tileValue > 0) {
+                    const checkerElem = document.createElement("div");
+                    checkerElem.classList.add("checker");
+                    checkerElem.classList.add(`player-${tileValue}`);
+                    tileElem.appendChild(checkerElem);
+                }
+                this.elem.appendChild(tileElem);
+                tileElem.addEventListener("click", () => { this.dropPiece(x) });
+            }
+        }
+    }
+  
+    getTile(x, y){
+        if (x < 0 || y < 0 || x >= this.width || y >= this.height) return;
+        return this.tiles[y * this.width + x];
+    }
+  
+    dropPiece(column){
+        for (let y = this.height - 1; y >= 0; y--) {
+            if (this.getTile(column, y) === 0) {
+                this.tiles[y * this.width + column] = this.currentPlayer;
+                this.draw();
+            if (this.checkWin(column, y)) {
+                this.tiles[y * this.width + column] = this.currentPlayer;
+                this.draw();
+                alert(`Player ${this.currentPlayer} wins!`);
+                this.reset(); // resets the board/game
+            } 
+            return this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+            }
+        }
+    }
+  
+    checkWin(column, row){
+        const directions=[
+          [0, 1],  // Vertical
+          [1, 0],  // Horizontal
+          [1, 1],  // Diagonal (top-left to bottom-right)
+          [-1, 1] // Diagonal (top-right to bottom-left)
+        ];
+        const currentPlayer = this.tiles[row * this.width + column];
+      
+        for (const [dx, dy] of directions){
+            let count = 1;
+            // Check in both directions
+            for (const dir of [-1, 1]){
+                let cx = column + dir * dx;
+                let cy = row + dir * dy;
+                while ( cx >= 0 && cy >= 0 && cx < this.width && cy < this.height && this.tiles[cy * this.width + cx] === currentPlayer ){
+                    count++;
+                    cx += dir * dx;
+                    cy += dir * dy;
+                }
+            }
+                if (count >= 4) { return true } // Win condition
+        }
+        return false; // No win condition found
+    }
+      
+    reset() {
+        this.tiles.fill(0);
+        this.currentPlayer = 1;
+        this.draw();
+    }
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+import { Board } from "./board.js"
+
+
+const main = () => {
+    const board = new Board(7, 6);
+    board.draw();
+}
+
+main();
